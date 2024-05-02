fafa558c5fad03aba40e65252dcda176e22d1f16
Karthikeya <145262370+weasel-0@users.noreply.github.com>
2024-05-02 22:38:17 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Pokedex from '$root/components/Pokedex.svelte'
+</script>
+
+<Pokedex />
---
+++ src/components/Pokedex.svelte
@@ -0,0 +1,10 @@
+<script>
+    let pokemonName = ''
+
+    function logIt() {
+        console.log(pokemonName)
+    }
+</script>
+
+<input type="text" bind:value={pokemonName} placeholder="Enter Pokemon name" />
+<button on:click={logIt}>Search</button>
